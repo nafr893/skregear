051496c2db87c515baf1b3ff005be364aac69631
nafr893 <natefreyhof23@gmail.com>
2026-04-22 09:09:38 -0500

    Adjust Predictive Search Drawer v2

--- assets/predictive-search.js
+++ assets/predictive-search.js
@@ -315,7 +315,9 @@ class PredictiveSearchComponent extends Component {
 
     const url = new URL(Theme.routes.predictive_search_url, location.origin);
     url.searchParams.set('q', searchTerm);
+    url.searchParams.set('resources[type]', 'product,article,query');
     url.searchParams.set('resources[limit_scope]', 'each');
+    url.searchParams.set('resources[limit]', '8');
 
     const { predictiveSearchResults } = this.refs;
 
